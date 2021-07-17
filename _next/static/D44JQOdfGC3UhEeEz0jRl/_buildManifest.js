f19bafb9898f320ca21d2f1aa8c6d20779d37c57
Tom <gh286991@gmail.com>
2021-07-18 00:51:13 +0800

    Updates

---
+++ _next/static/D44JQOdfGC3UhEeEz0jRl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8e27c432a51e0b0678dd.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],"/about":["static/chunks/pages/about-d8c536a51f16902185c3.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
